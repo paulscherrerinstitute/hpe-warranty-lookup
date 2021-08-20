823483f717af89999a2876bec2e617431fb7b172
Leonardo Sala <leonardo.sala@psi.ch>
2021-08-20 14:57:01 +0200

    json output, preparation for product number

--- hpe-warranty-lookup.py
+++ hpe-warranty-lookup.py
@@ -8,7 +8,8 @@ import argparse
 def get_warranty_HTML(serial):
     params = urllib.parse.urlencode({
         'rows[0].item.serialNumber': serial,
-        'rows[0].item.countryCode': "US"
+        'rows[0].item.countryCode': "US",
+        # 'rows[0].item.productNumber': "719064-B21"
         })
     headers = {"Content-type": "application/x-www-form-urlencoded"}
 
@@ -18,6 +19,7 @@ def get_warranty_HTML(serial):
         sys.exit(response.status_code, response.content)
 
     data = response.content
+    # print(data)
     return extract_warranty_info(data)
     # return data
 
@@ -26,8 +28,16 @@ def extract_warranty_info(html):
     active_warranties = []
     soup = BeautifulSoup(html, 'html.parser')
     active = soup.find_all("td", attrs={"style": 'color: Green'}, string="Active")
+    expired = soup.find_all("td", attrs={"style": 'color: Red'}, string="Expired")
 
     for td in active:
+        warranty = {
+            "service_type" : td.previous_sibling.previous_sibling.previous_sibling.get_text(strip=True),
+            "start_date"   : td.previous_sibling.previous_sibling.string,
+            "end_date"     : td.previous_sibling.string
+        }
+        active_warranties.append(warranty)
+    for td in expired:
         warranty = {
             "service_type" : td.previous_sibling.previous_sibling.previous_sibling.get_text(strip=True),
             "start_date"   : td.previous_sibling.previous_sibling.string,
@@ -39,11 +49,18 @@ def extract_warranty_info(html):
 
 
 if __name__ == "__main__":
+    import json
     parser = argparse.ArgumentParser(usage="Small tool to retrieve HPE harware warranty status, using the serial number")
-    parser.add_argument("serial", type=str, help="serial number of the HPE hardware")
+    parser.add_argument("serial", type=str, help="serial number of the HPE hardware", nargs="+")
     # parser.add_argument("--model", type=str, help="model number of HPE harware")
     args = parser.parse_args()
 
-    print(get_warranty_HTML(args.serial))
+    systems = {}
+    for serial in args.serial:
+        # print(serial)
+
+        systems[serial] = get_warranty_HTML(serial)
+
+    print(json.dumps(systems))
 
     # main(sys.argv)
